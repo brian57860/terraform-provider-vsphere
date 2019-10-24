cf174db439caa848f372de5a2d743f11cfde6cd0
brian57860 <53904715+brian57860@users.noreply.github.com>
2019-10-24 11:03:09 +0100

    24/10/2019

--- vsphere/data_source_vsphere_virtual_machine.go
+++ vsphere/data_source_vsphere_virtual_machine.go
@@ -5,79 +5,33 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
+	"github.com/terraform-providers/terraform-provider-vsphere/vsphere/internal/helper/structure"
 	"github.com/terraform-providers/terraform-provider-vsphere/vsphere/internal/helper/virtualmachine"
 	"github.com/terraform-providers/terraform-provider-vsphere/vsphere/internal/virtualdevice"
 	"github.com/vmware/govmomi/object"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 func dataSourceVSphereVirtualMachine() *schema.Resource {
-	return &schema.Resource{
+	r := &schema.Resource{
 		Read: dataSourceVSphereVirtualMachineRead,
 
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:        schema.TypeString,
-				Description: "The name or path of the virtual machine.",
-				Required:    true,
-			},
-			"datacenter_id": {
-				Type:        schema.TypeString,
-				Description: "The managed object ID of the datacenter the virtual machine is in. This is not required when using ESXi directly, or if there is only one datacenter in your infrastructure.",
-				Optional:    true,
-			},
-			"scsi_controller_scan_count": {
-				Type:        schema.TypeInt,
-				Description: "The number of SCSI controllers to scan for disk sizes and controller types on.",
-				Optional:    true,
-				Default:     1,
-			},
-			"guest_id": {
-				Type:        schema.TypeString,
-				Description: "The guest ID of the virtual machine.",
-				Computed:    true,
-			},
-			"firmware": {
-				Type:        schema.TypeString,
-				Description: "The firmware type for this virtual machine.",
-				Computed:    true,
-			},
 			"alternate_guest_name": {
 				Type:        schema.TypeString,
 				Description: "The alternate guest name of the virtual machine when guest_id is a non-specific operating system, like otherGuest.",
 				Computed:    true,
 			},
-			"scsi_type": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The common SCSI bus type of all controllers on the virtual machine.",
-			},
-			"scsi_bus_sharing": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Mode for sharing the SCSI bus.",
-			},
-			"num_cpus": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     1,
-				Description: "The number of virtual processors to assign to this virtual machine.",
-			},
-			"num_cores_per_socket": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     1,
-				Description: "The number of cores to distribute amongst the CPUs in this virtual machine. If specified, the value supplied to num_cpus must be evenly divisible by this value.",
-			},
-			"memory": {
-				Type:        schema.TypeInt,
-				Optional:    true,
-				Default:     1024,
-				Description: "The size of the virtual machine's memory, in MB.",
-			},
 			"cpu_hot_add_enabled": {
 				Type:        schema.TypeBool,
 				Optional:    true,
 				Description: "Allow CPUs to be added to this virtual machine while it is running.",
+			},
+			"datacenter_id": {
+				Type:        schema.TypeString,
+				Description: "The managed object ID of the datacenter the virtual machine is in. This is not required when using ESXi directly, or if there is only one datacenter in your infrastructure.",
+				Optional:    true,
 			},
 			"disks": {
 				Type:        schema.TypeList,
@@ -100,26 +54,36 @@ func dataSourceVSphereVirtualMachine() *schema.Resource {
 					},
 				},
 			},
-			"network_interfaces": {
-				Type:        schema.TypeList,
-				Description: "The types of network interfaces found on the virtual machine, sorted by unit number.",
+			"enable_logging": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Enable logging on this virtual machine.",
+			},
+			"firmware": {
+				Type:        schema.TypeString,
+				Description: "The firmware type for this virtual machine.",
 				Computed:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"network_id": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"adapter_type": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"mac_address": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-					},
-				},
+			},
+			"guest_id": {
+				Type:        schema.TypeString,
+				Description: "The guest ID of the virtual machine.",
+				Computed:    true,
+			},
+			"memory": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     1024,
+				Description: "The size of the virtual machine's memory, in MB.",
+			},
+			"memory_hot_add_enabled": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Description: "Allow memory to be added to this virtual machine while it is running.",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "The name or path of the virtual machine.",
+				Required:    true,
 			},
 			"network_interface_types": {
 				Type:        schema.TypeList,
@@ -127,8 +91,88 @@ func dataSourceVSphereVirtualMachine() *schema.Resource {
 				Computed:    true,
 				Elem:        &schema.Schema{Type: schema.TypeString},
 			},
+			"network_interfaces": {
+				Type:        schema.TypeList,
+				Description: "The types of network interfaces found on the virtual machine, sorted by unit number.",
+				Computed:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"adapter_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"bandwidth_limit": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Default:      -1,
+							Description:  "The upper bandwidth limit of this network interface, in Mbits/sec.",
+							ValidateFunc: validation.IntAtLeast(-1),
+						},
+						"bandwidth_reservation": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Default:      0,
+							Description:  "The bandwidth reservation of this network interface, in Mbits/sec.",
+							ValidateFunc: validation.IntAtLeast(0),
+						},
+						"bandwidth_share_level": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							Default:      string(types.SharesLevelNormal),
+							Description:  "The bandwidth share allocation level for this interface. Can be one of low, normal, high, or custom.",
+							ValidateFunc: validation.StringInSlice(sharesLevelAllowedValues, false),
+						},
+						"bandwidth_share_count": {
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Computed:     true,
+							Description:  "The share count for this network interface when the share level is custom.",
+							ValidateFunc: validation.IntAtLeast(0),
+						},
+						"mac_address": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"num_cores_per_socket": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     1,
+				Description: "The number of cores to distribute amongst the CPUs in this virtual machine. If specified, the value supplied to num_cpus must be evenly divisible by this value.",
+			},
+			"num_cpus": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Default:     1,
+				Description: "The number of virtual processors to assign to this virtual machine.",
+			},
+			"scsi_bus_sharing": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Mode for sharing the SCSI bus.",
+			},
+			"scsi_controller_scan_count": {
+				Type:        schema.TypeInt,
+				Description: "The number of SCSI controllers to scan for disk sizes and controller types on.",
+				Optional:    true,
+				Default:     1,
+			},
+			"scsi_type": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The common SCSI bus type of all controllers on the virtual machine.",
+			},
 		},
 	}
+	structure.MergeSchema(r.Schema, schemaVirtualMachineResourceAllocation())
+
+	return r
 }
 
 func dataSourceVSphereVirtualMachineRead(d *schema.ResourceData, meta interface{}) error {
