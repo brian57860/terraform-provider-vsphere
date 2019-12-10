bb195e8e72eb1111b154bd1e673aabd073cf0925
brian57860 <53904715+brian57860@users.noreply.github.com>
2019-12-10 12:33:45 +0000

    10/12/2019

--- vsphere/resource_vsphere_virtual_machine.go
+++ vsphere/resource_vsphere_virtual_machine.go
@@ -1402,10 +1402,21 @@ func resourceVSphereVirtualMachineCreateInstantClone(d *schema.ResourceData, met
 	}
 	cfgSpec.DeviceChange = virtualdevice.AppendDeviceChangeSpec(cfgSpec.DeviceChange, delta...)
 
-	// Check whether any device operations either update or remove devices
-	// in which case we want to perform a reboot
+	// Check whether any device operations require a reboot
 	for _, deviceChange := range cfgSpec.DeviceChange {
-		if deviceChange.GetVirtualDeviceConfigSpec().Operation != types.VirtualDeviceConfigSpecOperationAdd {
+		_, cdrom := deviceChange.GetVirtualDeviceConfigSpec().Device.(*types.VirtualCdrom)
+		switch deviceChange.GetVirtualDeviceConfigSpec().Operation {
+		case types.VirtualDeviceConfigSpecOperationAdd:
+			if cdrom {
+				d.Set("reboot_required", true)
+				break
+			}
+		case types.VirtualDeviceConfigSpecOperationEdit:
+			if !cdrom {
+				d.Set("reboot_required", true)
+				break
+			}
+		case types.VirtualDeviceConfigSpecOperationRemove:
 			d.Set("reboot_required", true)
 			break
 		}
