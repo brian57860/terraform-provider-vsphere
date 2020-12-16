463ec0cd4e2912610bbecc0784eda4e0aaa8504e
brian57860 <53904715+brian57860@users.noreply.github.com>
2020-12-16 12:01:18 +0000

    Refactor Terraform Examples

--- examples/instantclone/static/variables.tf
+++ examples/instantclone/static/variables.tf
@@ -1,35 +1,35 @@
 // The datacenter the resources will be created in.
 variable "datacenter" {
-  type = "string"
+  type = string
 }
 
 // The name of the datastore the instant clone will be placed on
 variable "datastore" {
-  type = "string"
+  type = string
 }
 
 // The path to the folder to put this virtual machine in, relative to the datacenter that the resource pool is in
 variable "folder" {
-  type = "string"
+  type = string
 }
 
 // The number of instant clone child virtual machines to create
 variable "number_vms_required" {
-  type = "string"
+  type = string
 }
 
 // The resource pool the virtual machines will be placed in.
 variable "resource_pool" {
-  type = "string"
+  type = string
 }
 
 // The name of the source virtual machine to use when cloning.
 variable "source_name" {
-  type = "string"
+  type = string
 }
 
 // The number of instant clone child virtual machines to create
 variable "target_vm_prefix" {
-  type = "string"
+  type = string
 }
 
